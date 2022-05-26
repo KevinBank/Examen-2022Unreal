a3a3aee5a0a173504f88cb4fc7f935f7740c5993
Kevin <Kevinb2002@live.nl>
2022-05-26 21:10:24 +0200

    Made steam multiplayer ready for testing

---
+++ Source/Examen2022/Examen2022.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Examen2022.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Examen2022, "Examen2022" );
